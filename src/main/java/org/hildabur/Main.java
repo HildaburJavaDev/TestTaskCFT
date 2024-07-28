996f494244cf9b0f486e963ba7d3cde03a790cdb
HildaburJavaDev <hildabur.javadev@gmail.com>
2024-07-28 14:41:19 +0700

    finish stats for datatypes

--- src/main/java/org/hildabur/Main.java
+++ src/main/java/org/hildabur/Main.java
@@ -1,12 +1,18 @@
 package org.hildabur;
 
+import org.hildabur.models.FloatStats;
+import org.hildabur.models.IntegerStats;
+import org.hildabur.models.Stats;
+import org.hildabur.models.StringsStats;
 import org.hildabur.services.ArgumentProvider;
 import org.hildabur.services.DirectoryManager;
 import org.hildabur.services.DirectoryProvider;
 import org.hildabur.services.FileProcessorService;
 import org.hildabur.storage.ArgumentStorage;
+import org.hildabur.utils.DataType;
 import org.hildabur.utils.Notificator;
 
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -22,11 +28,28 @@ public class Main {
             Notificator.printWarning("Invalid path. Result will be in default location");
             argumentStorage.setFilepath("");
         } else {
+            ConcurrentHashMap<DataType, Stats> statsMap = new ConcurrentHashMap<>();
+            initializeStatsMap(statsMap);
+
             ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
             for (String fileName: argumentStorage.getFiles()) {
-                executorService.execute(new FileProcessorService(argumentStorage, directoryManager.getDirectoryProvider(), fileName));
+                executorService.execute(new FileProcessorService(argumentStorage, directoryManager.getDirectoryProvider(), fileName, statsMap));
             }
             executorService.shutdown();
+            printStats(statsMap, argumentStorage.isOptionF());
         }
+    }
+
+    private static void initializeStatsMap(ConcurrentHashMap<DataType, Stats> statsMap) {
+        statsMap.put(DataType.INTEGER, new IntegerStats());
+        statsMap.put(DataType.FLOAT, new FloatStats());
+        statsMap.put(DataType.STRING, new StringsStats());
+    }
+    private static void printStats(ConcurrentHashMap<DataType, Stats> statsMap, boolean isOutFullStats) {
+        statsMap.forEach(
+                (key, stats) -> System.out.println(isOutFullStats
+                    ? stats.getFullStats()
+                    : stats.getShortStats(key.getTypeName().toLowerCase()))
+        );
     }
 }
--- src/main/java/org/hildabur/services/FileProcessorService.java
+++ src/main/java/org/hildabur/services/FileProcessorService.java
@@ -1,22 +1,30 @@
 package org.hildabur.services;
 
+import org.hildabur.models.Stats;
 import org.hildabur.storage.ArgumentStorage;
+import org.hildabur.utils.DataType;
+import org.hildabur.utils.DataTypeDeterminer;
 import org.hildabur.utils.Notificator;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class FileProcessorService implements Runnable{
+public class FileProcessorService implements Runnable {
     private final ArgumentStorage argumentStorage;
     private final DirectoryProvider directoryProvider;
     private final String fileName;
+    private final ConcurrentHashMap<DataType, Stats> statsMap;
 
-    public FileProcessorService(ArgumentStorage argumentStorage, DirectoryProvider directoryProvider, String fileName) {
+    public FileProcessorService(ArgumentStorage argumentStorage, DirectoryProvider directoryProvider, String fileName, ConcurrentHashMap<DataType, Stats> statsMap) {
         this.argumentStorage = argumentStorage;
         this.directoryProvider = directoryProvider;
         this.fileName = fileName;
+        this.statsMap = statsMap;
     }
 
     @Override
@@ -24,12 +32,24 @@ public class FileProcessorService implements Runnable{
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                updateDataTypeStats(DataTypeDeterminer.determineDataType(line), line);
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException exception) {
             Notificator.printWarning("Error: The file '" + fileName + "' was not found. Please check the file path and try again.");
-        } catch (IOException e) {
+        } catch (IOException exception) {
             Notificator.printWarning("Error: An unexpected error occurred while processing the file '" + fileName + "'.");
+        }
+    }
+
+    private void updateDataTypeStats(DataType dataType, String line) {
+        Stats stats = statsMap.get(dataType);
+        if (stats != null) {
+            switch (dataType) {
+                case INTEGER -> stats.updateStats(new BigInteger(line));
+                case FLOAT -> stats.updateStats(new BigDecimal(line));
+                case STRING -> stats.updateStats(line);
+                default -> Notificator.printWarning("Unknown data type " + line);
+            }
         }
     }
 }
